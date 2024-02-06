f94d7e20eaeef12d33fd9da6c89e489a211f828c
azeddine chakir <77072940+hcw5@users.noreply.github.com>
2024-02-06 23:37:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-import os
-from flask import Flask
-from flask import render_template, jsonify
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return jsonify({'message': 'ok'})
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == '__main__':
+    app.run(debug=True)
